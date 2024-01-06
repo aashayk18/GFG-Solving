00212c4e3ddb97da85053a487d9fa28b9dd23887
Aashay Kulkarni <72932792+aashayk18@users.noreply.github.com>
2024-01-06 10:03:24 +0530

    Create Nth Natural Number.java

---
+++ Nth Natural Number.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner ob=new Scanner(System.in);
+		int t=ob.nextInt();
+		while(t-->0)
+		{
+		    long n=ob.nextLong();
+		    Solution ab=new Solution();
+		    long k=ab.findNth(n);
+		    System.out.println(k);
+		}
+	}
+}
+
+    
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    long findNth(long N)
+    {
+        //code here
+        long ans = 0;
+        long p = 1;
+        
+        while (N > 0) {
+            ans = ans + p * (N % 9);
+            N = N / 9;
+            p *= 10;
+        }
+        return ans;
+    }
+}
